35aece15ce9acde11c074c135e4e7cd97c42ecc8
David Jia <105570809@qq.com>
2023-10-24 21:44:42 +0800

    add new code

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:uuid/uuid.dart';
 
+import 'dart:developer' as devtool show log;
+
 void main() {
   runApp(
     ChangeNotifierProvider(
@@ -17,7 +19,7 @@ void main() {
         home: const HomePage(),
         debugShowCheckedModeBanner: false,
         routes: {
-          '/new': (context) => const Material(),
+          '/new': (context) => const NewBreadCrumbWidget(),
         },
       ),
     ),
@@ -63,22 +65,31 @@ class BreadCrumbProvider extends ChangeNotifier {
   }
 }
 
+typedef OnBreadCrumbTapped = void Function(BreadCrumb);
+
 class BreadCrumbsWidget extends StatelessWidget {
+  final OnBreadCrumbTapped onTapped;
   final UnmodifiableListView<BreadCrumb> breadCrumbs;
 
   const BreadCrumbsWidget({
     super.key,
     required this.breadCrumbs,
+    required this.onTapped,
   });
 
   @override
   Widget build(BuildContext context) {
     return Wrap(
-      children: breadCrumbs.map((element) {
-        return Text(
-          element.title,
-          style: TextStyle(
-            color: element.isActive ? Colors.blue : Colors.black,
+      children: breadCrumbs.map((breadCrumb) {
+        return GestureDetector(
+          onTap: () {
+            onTapped(breadCrumb);
+          },
+          child: Text(
+            breadCrumb.title,
+            style: TextStyle(
+              color: breadCrumb.isActive ? Colors.blue : Colors.black,
+            ),
           ),
         );
       }).toList(),
@@ -98,6 +109,16 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
+          Consumer<BreadCrumbProvider>(
+            builder: (context, value, child) {
+              return BreadCrumbsWidget(
+                breadCrumbs: value.items,
+                onTapped: (breadCrumb) {
+                  devtool.log('${breadCrumb.name} tapped');
+                },
+              );
+            },
+          ),
           TextButton(
             onPressed: () {
               Navigator.of(context).pushNamed('/new');
@@ -109,6 +130,59 @@ class HomePage extends StatelessWidget {
               context.read<BreadCrumbProvider>().reset();
             },
             child: const Text('Reset'),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NewBreadCrumbWidget extends StatefulWidget {
+  const NewBreadCrumbWidget({super.key});
+
+  @override
+  State<NewBreadCrumbWidget> createState() => _NewBreadCrumbWidgetState();
+}
+
+class _NewBreadCrumbWidgetState extends State<NewBreadCrumbWidget> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    _controller = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add new bread crumb'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: const InputDecoration(
+              hintText: 'Enter a new bread crumb here...',
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              final text = _controller.text;
+              if (text.isNotEmpty) {
+                final breadCrumb = BreadCrumb(isActive: false, name: text);
+                context.read<BreadCrumbProvider>().add(breadCrumb);
+                Navigator.of(context).pop();
+              }
+            },
+            child: const Text('Add'),
           ),
         ],
       ),
